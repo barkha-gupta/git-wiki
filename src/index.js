ec990a7c598041b95edd395e46c82ea8ca691a59
barkha-gupta <barkhagupta401@gmail.com>
2026-09-04 23:44:21 +0530

    feat: add inngest server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
 const app = express();
+app.use(express.json());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("server setup");
